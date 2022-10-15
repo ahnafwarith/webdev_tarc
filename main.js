805b09590dbd38e1902b482a66c19a6b3e7081c5
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-10-15 19:21:09 +0600

    1st class

---
+++ main.js
@@ -0,0 +1,33 @@
+/* Class of Web Dev Adv */
+let name = 'Zarif'
+const pi = 3.146
+// pi = 'jfdi'
+console.log(name, pi)
+
+/* Push POP */
+const arr = [1, 3, 5, 7];
+arr.push(9)
+console.log(arr)
+arr.pop()
+console.log(arr)
+
+/* Spread OPerator */
+console.log(...arr)
+
+/* Array Destructuring */
+const [st, se, th] = arr
+console.log(se)
+
+/*--------------
+ Object 
+ ----------------*/
+const student = {
+    name: 'Ahnaf',
+    id: 22101802,
+    sem: 3
+}
+
+student['home'] = 'dhaka'
+console.log(student.home)
+
+/* Object Destructuring */
